811770952ae23816e9da203062ca68541b006092
JVAdam <100501051+JVAdam@users.noreply.github.com>
2025-10-17 17:48:29 -0300

    auto moving

--- Scripts/controller.gd
+++ Scripts/controller.gd
@@ -1,7 +1,24 @@
 extends Node
 
-var foods = []
+var nearest = Vector2(0,0)
+var distance = Vector2(0,0)
+var pos = Vector2(0,0)
 
-func find_food(body: CharacterBody2D):
+func find_food(body: CharacterBody2D) -> Vector2:
 	print(body.get_tree().get_nodes_in_group("food"))
+	print(body.global_position)
+	for i in range(body.get_tree().get_nodes_in_group("food").size()):
+		var item = body.get_tree().get_nodes_in_group("food")[i]
+		if(i == 0):
+			distance = body.global_position - item.global_position
+			nearest = distance
+			pos = item.global_position
+		else:
+			distance = body.global_position - item.global_position
+			if(nearest > distance):
+				nearest = distance
+				pos = item.global_position
+		print(item.global_position)
+	print(pos)
 	
+	return pos
